32a76d768773c11e7592402121e77676b553a81c
Aakriti_Kapoor <akapoor1465@conestogac.on.ca>
2021-06-04 14:42:27 -0400

    New Branch changes Start here

--- CalcTest/UnitTest1.cs
+++ CalcTest/UnitTest1.cs
@@ -1,5 +1,6 @@
 using NUnit.Framework;
 using AwesomeCalculator;
+using System.ComponentModel;
 
 namespace CalcTest
 {
@@ -9,7 +10,7 @@ namespace CalcTest
              
         Calc calc = new Calc();   
 
-        [Test]
+        [Test(Description = "Adding two positive integers")]
         public void Addition_of_two_integers()
         {
             double a = 5, b = 4;//arrange
@@ -17,6 +18,45 @@ namespace CalcTest
             calc.SetSecondNumber(b);//arrange
             double res = calc.GetAddition(); //act
             Assert.AreEqual(res, 9); //assert
+        }
+
+        [Test(Description = "Substraction two posiive integers")]
+        public void Substraction_of_two_Integer()
+        {
+            // ARRANGE
+            double a = 89, b = 4;
+            // ACT
+            calc.SetFirstNumber(a);
+            calc.SetSecondNumber(b);
+            double actual = calc.GetSubtraction();
+            // ASSERT
+            Assert.AreEqual(actual,85);
+            Assert.Positive(actual);
+
+        }
+
+        [Test(Description = "Substraction two equal posiive integers")]
+        public void Substraction_of_two_equal_Integer()
+        {
+            // ARRANGE
+            double a = 4, b = 4;
+            // ACT
+            calc.SetFirstNumber(a);
+            calc.SetSecondNumber(b);
+            double actual = calc.GetSubtraction();
+            // ASSERT
+            Assert.Zero(actual);
+
+        }
+        [Test(Description = "Multiplication two positive integers")]
+        public void Multiplication_of_two_Integers()
+        {
+
+        }
+        [Test(Description ="Division of two positive integer")]
+         public void Division_of_two_Integers()
+        {
+
         }
 
 
